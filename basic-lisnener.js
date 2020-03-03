4cae8c3905d778bace28564a674ec354bab3c939
Ankur Tiwari <atiwari@basecampscout.com>
2020-03-03 10:09:21 +0530

    Initial commit

---
+++ basic-lisnener.js
@@ -0,0 +1,28 @@
+var net    = require('net'); // to create a TCP server
+const axios = require('axios'); //to hit curl request of our server
+var moment = require('moment'); // time related opreation should be manage with this package
+
+var HOST   = '0.0.0.0';//localhost
+var PORT   = 5008;//port that allow on the server to communicate with device
+
+var fs = require("fs");//to write logs in our file
+
+console.log("############################ Listner Is Running ############################", HOST, PORT);
+
+//create a TCP server to read data sending from the device
+net.createServer(function(sock) {
+    //if device connected and sending data on socket then you can find it here
+    sock.on('data', function(data) {
+        //converte any kind of data into human readable form with the help  utf8 charset
+        var result = data.toString('utf8');
+        //Read the device manual carefully and you can split string on the basic of that
+    });
+
+}).listen(PORT, HOST);
+
+//function to write logs in our file
+function write_log(write_content) {
+	fs.appendFile('/var/www/fleetpro/log_fbm.txt', write_content, function (err) {
+		if (err) { throw err; }
+	});
+}
